ab9905d2ff6adc40704647f04732c7a17844eab4
Linus Lang <linlagode@gmail.com>
2025-10-28 20:33:04 +0100

    update urls

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", userRoutes);
-app.use("/api", taskRoutes);
-app.use("/api", projectRoutes);
-app.use("/api", subTaskRoutes);
-app.use("/api", milestoneRoutes);
+app.use("/user/api", userRoutes);
+app.use("/task/api", taskRoutes);
+app.use("/project/api", projectRoutes);
+app.use("/subtask/api", subTaskRoutes);
+app.use("/milestone/api", milestoneRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
